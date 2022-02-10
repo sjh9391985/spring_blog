bfbd788e6de6dbc2737a49f9abb9666a571c869b
손재현 - 개발 <sonjaehyeon@sonjaehyeon-ui-MacBookPro.local>
2022-02-10 15:56:36 +0900

    properties -> yml 변경

---
+++ src/main/java/com/springboot/springboot/test/TempControllerTest.java
@@ -0,0 +1,16 @@
+package com.springboot.springboot.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TempControllerTest {
+
+    @GetMapping("/temp/home")
+    public String tempHome(){
+
+        System.out.println("temp home");
+        return "/home.html";
+    }
+
+}
